3d3bc4e872240c5a411e16f22888a1015091a98d
Rezgar Shakeri <Rezgar.Shakeri@colorado.edu>
2020-11-14 12:06:33 -0700

    FEcreateBasis.m: checked with PETSc and works for arbitrary P and Q

--- FEcreateBasis.m
+++ FEcreateBasis.m
@@ -1,41 +1,22 @@
 function [B, D, W, Q] = FEcreateBasis(P,Q, Qmode)
 
 
- %[nodes, ~ ] = LobattoQuadrature(P); 
+ [nodes, ~ ] = LobattoQuadrature(P); 
  if(strcmp(Qmode, 'GAUSS'))
      %extend this to higher order than 2 based on C code later
      [qref1d, W] = GaussQuadrature(Q);
-     x = qref1d;
-     if P==2
-         BHat = [(1-x)/2; (1+x)/2];
-         DHat = [-1/2+0*x; 1/2+0*x];
-     elseif P==3
-         BHat = [(x.^2 - x)/2; (1-x.^2); (x.^2+x)/2];
-         DHat = [x-1/2; -2*x; x+1/2];
-     else
-         error('Polynomial order is not supported!');
-     end     
+     [B1d, D1d] = FEBasisEval(P, Q, nodes, qref1d);
 
  elseif(strcmp(Qmode, 'LGL'))
-     [qref1d, W] = LobattoQuadrature(Q-1);     
-     x = qref1d;
-     if P==2
-         BHat = [(1-x)/2; (1+x)/2];
-         DHat = [-1/2+0*x; 1/2+0*x];
-     elseif P==3
-         BHat = [(x.^2 - x)/2; (1-x.^2); (x.^2+x)/2];
-         DHat = [x-1/2; -2*x; x+1/2];
-     else
-         error('Polynomial order is not supported!');
-     end 
+     [qref1d, W] = LobattoQuadrature(Q);     
+     [B1d, D1d] = FEBasisEval(P, Q, nodes, qref1d);
  else
      error('Qmode error! Choose GAUSS or LGL Quadrature points!');
  end
 
 
- B = reshape(BHat,P*Q,1)';
- D = reshape(DHat,P*Q,1)';
- W = W;
+ B = B1d;
+ D = D1d;
  Q = qref1d;
 
 end
@@ -74,11 +55,11 @@ function [qref1d, W] = LobattoQuadrature(Q)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Truncation + 1
-    Q1=Q+1;
+% Truncation
+    Q1=Q;
 
     % Use the Chebyshev-Gauss-Lobatto nodes as the first guess
-    x=-cos(pi*(0:Q)/Q)';
+    x=-cos(pi*(0:Q-1)/(Q-1))';
 
     % The Legendre Vandermonde Matrix
     P=zeros(Q1,Q1);
@@ -95,16 +76,46 @@ function [qref1d, W] = LobattoQuadrature(Q)
 
         P(:,1)=1;    P(:,2)=x;
 
-        for k=2:Q
+        for k=2:Q-1
             P(:,k+1)=( (2*k-1)*x.*P(:,k)-(k-1)*P(:,k-1) )/k;
         end
 
-        x=xold-( x.*P(:,Q1)-P(:,Q) )./( Q1*P(:,Q1) );
+        x=xold-( x.*P(:,Q1)-P(:,Q-1) )./( Q1*P(:,Q1) );
 
     end
 
-    w=2./(Q*Q1*P(:,Q1).^2);
+    w=2./((Q-1)*Q1*P(:,Q1).^2);
     W = w';
     qref1d = x';
     
  end
+
+
+function [B1d, D1d] = FEBasisEval(P, Q, nodes, qref1d)
+
+ B1d = zeros(1,P*Q);
+ D1d = zeros(1,P*Q);
+
+for i = 1:Q
+    c1 = 1.0;
+    c3 = nodes(1) - qref1d(i);
+    B1d((i-1)*P+1) = 1.0;
+    for j = 2:P
+        c2 = 1.0;
+        c4 = c3;
+        c3 = nodes(j) - qref1d(i);
+            for k = 1:j-1
+                dx = nodes(j) - nodes(k);
+                c2 = c2*dx;
+                if k == j-1
+                    D1d((i-1)*P + j) = c1*(B1d((i-1)*P + k) - c4*D1d((i-1)*P + k)) / c2;
+                    B1d((i-1)*P + j) = - c1*c4*B1d((i-1)*P + k) / c2;
+                end
+                D1d((i-1)*P + k) = (c3*D1d((i-1)*P + k) - B1d((i-1)*P + k)) / dx;
+                B1d((i-1)*P + k) = c3*B1d((i-1)*P + k) / dx;
+            end
+        c1 = c2;
+    end
+end
+
+end
