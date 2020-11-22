d87886c90a747dfc2112a92b371ee09b53858f5e
Arash <arashm81@gmail.com>
2020-11-21 19:48:53 -0800

    GLL clean up.

--- FEcreateBasis.m
+++ FEcreateBasis.m
@@ -18,22 +18,22 @@ function [B, D, W, qref1d] = FEcreateBasis(P,Q, Qmode)
      [qref1d, W] = GaussQuadrature(Q);
      % get the Basis and its derivative as an 1xPQ array
      [B1d, D1d] = FEBasisEval(P, Q, nodes, qref1d);
- elseif(strcmp(Qmode, 'LGL'))
+ elseif(strcmp(Qmode, 'GLL'))
      % get the quadrature points and weights using LGL
      [qref1d, W] = LobattoQuadrature(Q);   
      % get the Basis and its derivative as an 1xPQ array  
      [B1d, D1d] = FEBasisEval(P, Q, nodes, qref1d);
  else
-     error('Qmode error! Choose GAUSS or LGL Quadrature points!');
+     error('Qmode error! Choose GAUSS or GLL Quadrature points!');
  end
  B = B1d;
  D = D1d;
 end
 
 function [qref1d,w] = GaussQuadrature(Q)
-%input: Q: Number of quadrature points (Gauss)
-%output:qref1d: Gauss quadrature points
-%       W: Gauss weights
+%input  : Q: Number of quadrature points (Gauss)
+%output : qref1d: Gauss quadrature points in 1D
+%         W: Gauss weights
 % Golub-Welsch algorithm: (Brute force version by Trefethen-Bau)
 % to calculate Gauss points and weights using Legendre weight function 
 %
@@ -45,57 +45,28 @@ function [qref1d,w] = GaussQuadrature(Q)
 end
 
 function [qref1d, W] = LobattoQuadrature(Q)
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%
-% Computes the Legendre-Gauss-Lobatto nodes, weights and the LGL Vandermonde
-% matrix. The LGL nodes are the zeros of (1-x^2)*P'_Q(x). Useful for numerical
-% integration and spectral methods.
-%
-% Reference on LGL nodes and weights:
-%   C. Canuto, M. Y. Hussaini, A. Quarteroni, T. A. Tang, "Spectral Methods
-%   in Fluid Dynamics," Section 2.3. Springer-Verlag 1987
-%
-% Written by Greg von Winckel - 04/17/2004
-% Contact: gregvw@chtm.unm.edu
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Truncation
-    Q1=Q;
-
     % Use the Chebyshev-Gauss-Lobatto nodes as the first guess
     x=-cos(pi*(0:Q-1)/(Q-1))';
-
     % The Legendre Vandermonde Matrix
-    P=zeros(Q1,Q1);
-
+    Vmat=zeros(Q,Q);
     % Compute P_(Q) using the recursion relation
     % Compute its first and second derivatives and
     % update x using the Newton-Raphson method.
-
     xold=2;
 
     while max(abs(x-xold))>eps
-
         xold=x;
-
-        P(:,1)=1;    P(:,2)=x;
-
+        Vmat(:,1)=1; 
+        Vmat(:,2)=x;
         for k=2:Q-1
-            P(:,k+1)=( (2*k-1)*x.*P(:,k)-(k-1)*P(:,k-1) )/k;
+            Vmat(:,k+1)=( (2*k-1)*x.*Vmat(:,k)-(k-1)*Vmat(:,k-1) )/k;
         end
-
-        x=xold-( x.*P(:,Q1)-P(:,Q-1) )./( Q1*P(:,Q1) );
-
+        x=xold-( x.*Vmat(:,Q)-Vmat(:,Q-1))./( Q*Vmat(:,Q));
     end
 
-    w=2./((Q-1)*Q1*P(:,Q1).^2);
-    W = w';
-    qref1d = x';
-    
- end
+    W=(2./((Q-1)*Q*Vmat(:,Q).^2))';
+    qref1d = x';    
+end
 
 
 function [B1d, D1d] = FEBasisEval(P, Q, nodes, qref1d)
