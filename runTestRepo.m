57058c0f419f5a5ee040dc4d5eeee2a10e38147c
Arash <arashm81@gmail.com>
2020-12-01 17:41:17 -0800

    added Basis tests

--- runTestRepo.m
+++ runTestRepo.m
@@ -2,6 +2,7 @@
 clear
 clc
 
+%DM functions
 DM = DMcreateFromFile('multi-block.exo');
 DM
 DM = DMrenameMaterialBlock(DM, 'materialBlock3', 'Aluminum');
@@ -17,3 +18,26 @@ DM.wall
 DM.ns_20
 DM.ForceFace
 DM.ss_11
+
+%The elements in the above DM are serentipity. They won't work with Tensor
+% FE basis functions
+DM = DMcreateFromFile('beam_12e_ns_ss.exo');
+DM
+P = 2;
+Q = 5;
+[B1d, D1d, W, qref1d] = FEcreateBasis(P,Q, 'GAUSS');
+B1d
+D1d
+W
+qref1d
+[B_hat,D_hat, W_hat] = createTensor(B1d,D1d,W,P,Q,DM.dim);
+B_hat
+D_hat
+W_hat
+
+element_vtx_coords = DM.coords(DM.materialBlock1.conn(1,:),:);
+[dets, inverseElemVtxJacobian] = invJacobianTensor(element_vtx_coords, D_hat, Q,DM.dim);
+%You get NaN's becasue the element_vtx_coords is not ordered
+%lexicographically
+dets
+inverseElemVtxJacobian
